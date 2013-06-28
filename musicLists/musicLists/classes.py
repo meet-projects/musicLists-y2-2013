44592474ae427c3dbb97f78c76d3b1a917e2e321
Sagi Zur <sagi12@cs.huji.ac.il>
2013-06-28 12:02:04 +0300

    working on models

--- musicLists/musicLists/classes.py
+++ musicLists/musicLists/classes.py
@@ -1,1 +1,21 @@
-import Django*
+from django.db import models
+
+class user(models.Model):
+    firstname=CharField(max_length=20)
+    lastname=CharField(max_length=20)
+    nickname=CharField(max_length=15)
+    password=CharField(min_length=6, max_length=20)
+    birthday=models.DateField(blank=True, null=True)
+    favgenres=ForeignKey('genre')
+    favalbums=ForeingKey('album)
+    friends=ForeignKey('user')
+    favsongs=ForeignKey('song')
+    favartists=ForeignKey('artist')
+
+class song(models.Model):
+    songname=CharField(max_length=1000)
+    artist=ForeignKey('artist')
+    genre=
+
+
+
