4349041f4831c788a7a07c38fa3975e8f51f308b
Sagi Zur <sagi12@cs.huji.ac.il>
2013-07-01 18:24:01 +0300

    Made the classes

--- musicLists/music/models.py
+++ musicLists/music/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Guy(models.Model):
+    user=models.ForeignKey('User')
+    favsongs=models.ManyToManyField('song')
+    favartists=models.ManyToManyField('artist')
+    favgenres=models.ManyToManyField('genre')
+    def __unicode__(self):
+        return self.user.username
+
+class Song(models.Model):
+    name=models.CharField(max_length=200)
+    artist=models.ForeignKey('Artist')
+    genres=models.ManyToManyField('genre')
+    album=models.ForeignKey('album')
+    link=models.CharField(max_length=2000)
+    def __unicode__(self):
+        return self.name
+
+class Artist(models.Model):
+    name=CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
+
+class Album(models.Model):
+    artist=models.ForeignKey('artist')
+    genres=models.ManyToManyField('genre')
+    
+    
+class Genre(models.Model):
+    name=models.CharField(max_length=200)
+    
+`
