887c7e95e160f7be7941bad5b42cdae8f250744a
Sagi Zur <sagi12@cs.huji.ac.il>
2013-07-01 18:44:47 +0300

    finished the classes!

--- musicLists/music/models.py
+++ musicLists/music/models.py
@@ -1,34 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class Guy(models.Model):
-    user=models.ForeignKey('User')
-    favsongs=models.ManyToManyField('song')
-    favartists=models.ManyToManyField('artist')
-    favgenres=models.ManyToManyField('genre')
-    def __unicode__(self):
-        return self.user.username
 
-class Song(models.Model):
+class Genre(models.Model):
     name=models.CharField(max_length=200)
-    artist=models.ForeignKey('Artist')
-    genres=models.ManyToManyField('genre')
-    album=models.ForeignKey('album')
-    link=models.CharField(max_length=2000)
-    def __unicode__(self):
-        return self.name
-
+   
 class Artist(models.Model):
-    name=CharField(max_length=200)
+    name=models.CharField(max_length=200)
     def __unicode__(self):
         return self.name
 
 class Album(models.Model):
-    artist=models.ForeignKey('artist')
-    genres=models.ManyToManyField('genre')
-    
-    
-class Genre(models.Model):
+    artist=models.ForeignKey('Artist')
+    genres=models.ManyToManyField('Genre')
+
+class Song(models.Model):
     name=models.CharField(max_length=200)
-    
-`
+    artist=models.ForeignKey('Artist')
+    genres=models.ManyToManyField('Genre')
+    album=models.ForeignKey('Album')
+    link=models.CharField(max_length=2000)
+    def __unicode__(self):
+        return self.name
+
+class Guy(models.Model):
+    user=models.OneToOneField(User)
+    favsongs=models.ManyToManyField('Song')
+    favartists=models.ManyToManyField('Artist')
+    favgenres=models.ManyToManyField('Genre')
+    def __unicode__(self):
+        return self.user.username
