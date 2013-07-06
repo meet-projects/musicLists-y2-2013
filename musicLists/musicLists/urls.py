d9f032ae5039bd3a0949e0802933346203ce91c0
Sagi Zur <sagi12@cs.huji.ac.il>
2013-07-06 12:48:14 +0300

    added an addartist and addalbum

--- musicLists/musicLists/urls.py
+++ musicLists/musicLists/urls.py
@@ -16,6 +16,10 @@ urlpatterns = patterns('',
 	url(r'^logout/$', views.logout_user),
 	url(r'^showaddsong/$', views.showaddsongs),
 	url(r'^makepost/$', views.add_post),
+	url(r'^addartist/$', views.addartist),
+	url(r'^showaddartist/$', views.showaddartist),
+	url(r'^addalbum/$', views.addalbum),
+	url(r'^showaddalbum/$', views.showaddalbum),
     # url(r'^musicLists/', include('musicLists.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
